d49429071bd1bae46b384ff54bfe01b1b3722a0a
Grgn07 <sitko.jana@mail.ru>
2024-03-20 11:49:27 +0300

    Create one file

---
+++ one.py
@@ -0,0 +1,7 @@
+side = 5
+perimetr = side * 4
+squart = side * side
+diag = side * 1.4
+print("Периметр:", round(perimetr, 1))
+print("Площадь:", round(squart, 1))
+print("Диагональ:", round(diag, 1))
